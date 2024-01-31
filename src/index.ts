6ff061ce998f5c1dd4bd178138dca3ad987e5e2f
Lucas Henrique <henrique.lucas521@gmail.com>
2024-01-31 00:33:11 -0300

    create frontend logic and data fetch

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import * as p from '@clack/prompts';
-import { setTimeout } from 'node:timers/promises';
 import color from 'picocolors';
+import * as clack from '@clack/prompts';
+import fs from 'fs';
 
 const main = async () => {
   console.clear();
 
-  p.intro(`${color.blue('Hello, welcome to the luk-modules CLI!')}`);
+  clack.intro(`${color.bold(color.blue('Hello, welcome to the luk-modules CLI!'))}`);
 
-  const projectType = await p.select({
+  // project type question
+  const projectType = await clack.select({
     message: `${color.green(
       color.bold('What type of project are you currently developing?')
     )}`,
@@ -17,7 +18,54 @@ const main = async () => {
     ],
   });
 
-  console.log(projectType);
+  // is using typescript question
+  const usingTypeScript = await clack.select({
+    message: `${color.green(color.bold('Are you using TypeScript?'))}`,
+    options: [
+      { value: true, label: 'Yes' },
+      { value: false, label: 'No' },
+    ],
+  });
+
+  // module name question
+  const moduleName = await clack.text({
+    message: `${color.green(color.bold('Type the name of the module:'))}`,
+  });
+
+  // module route question
+  const moduleRoute = await clack.text({
+    message: `${color.green(color.bold('Type the route to create the module'))}`,
+    placeholder: 'The route must exist',
+  });
+
+  if (projectType === 'frontend') {
+    await fs.mkdir(`${moduleRoute.toString()}/${moduleName.toString()}`, (err) => {
+      if (err) throw err;
+    });
+
+    await fs.writeFile(
+      usingTypeScript
+        ? `${moduleRoute.toString()}/${moduleName.toString()}/${moduleName.toString()}.ts`
+        : `${moduleRoute.toString()}/${moduleName.toString()}/${moduleName.toString()}.js`,
+      '',
+      (err) => {
+        if (err) throw err;
+      }
+    );
+
+    await fs.writeFile(
+      usingTypeScript
+        ? `${moduleRoute.toString()}/${moduleName.toString()}/index.ts`
+        : `${moduleRoute.toString()}/${moduleName.toString()}/index.js`,
+      '',
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+
+  if (projectType === 'backend') {
+  }
 };
 
 main();
